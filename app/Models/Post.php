6b7639debde3b6fd72979d004826d0ec81eec76a
Jhonan96 <j.a.h.f.2000@gmail.com>
2021-10-12 08:42:15 -0500

    Migraciones lista con datos de prueba y usuario SuperAdmin

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // relacion uno a muchos (User, Category)
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // relacion mucho a mucho con (tags)
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // relacion uno a uno con images
+
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+}
---
+++ app/Models/Tag.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    // relacion mucho a mucho con posts
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+}
